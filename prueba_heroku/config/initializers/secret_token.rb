9293eda077faf2920439ff6219cc6d1dec30e75c
ronzosblues <ronzo.orlandini@gmail.com>
2014-02-11 04:09:01 -0300

    First Commit

---
+++ prueba_heroku/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PruebaHeroku::Application.config.secret_key_base = 'c1b4bc0a0704b4f07ef70b3b67aa33800d77f02af5202545a55951a44571fa34ded4a9e4ee5bfab30265a6d7e0652723f6d09f713cb9bd08b9763cbb437159aa'
---
+++ prueba_heroku/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PruebaHeroku::Application.config.session_store :cookie_store, key: '_prueba_heroku_session'
